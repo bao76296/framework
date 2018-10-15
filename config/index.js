53b0c1f70c9d9c4e65b6224cb5ce1ad8b15286fd
bao76296 <762963131@qq.com>
2018-10-15 23:53:12 +0800

    'reset'

--- config/index.js
+++
@@ -1,8 +0,0 @@
-const server_config = require('./server_config');
-const webpack_config = require('./webpack_config')
-const config = {
-    server_config,
-    webpack_config
-}
-
-module.exports = config;
